5cbd783d190afbd2ccaed7ec30d484dea0939a4a
dddx <dingdangdogx@outlook.com>
2025-02-01 18:48:52 +0800

    feat: Implement client authentication and dynamic IP file handling

--- ip/whoisme.go
+++ ip/whoisme.go
@@ -1,15 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
+type Config struct {
+	CLIENTS []string `json:"CLIENTS"`
+}
+
+// Get json file content
+func loadConfig(filePath string) (*Config, error) {
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read config file: %v", err)
+	}
+
+	var config Config
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse config file: %v", err)
+	}
+
+	return &config, nil
+}
+
 // Processes the request and returns the client's IP address
 func whoisme(w http.ResponseWriter, r *http.Request) {
+	// load config
+	configs, err := loadConfig("config.json")
+	if err != nil {
+		log.Printf("Error loading config: %v \n", err)
+	}
+	id := r.URL.Query().Get("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+	key := r.URL.Query().Get("key")
+	if configs.CLIENTS[idInt] != key {
+		http.Error(w, "Key Error", http.StatusBadRequest)
+		return
+	}
+
 	// get the X-Forwarded-For field from the request header, if not, get the RemoteAddr
 	clientIP := r.Header.Get("X-Forwarded-For")
 	if clientIP == "" {
@@ -21,7 +60,7 @@ func whoisme(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// save IP to ip.last File
-	err := os.WriteFile("ip.last", []byte("Client IP: "+clientIP), 0644)
+	err = os.WriteFile(id+".ip", []byte("Client IP: "+clientIP), 0644)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error saving IP to file: %v", err), http.StatusInternalServerError)
 		return
@@ -33,7 +72,24 @@ func whoisme(w http.ResponseWriter, r *http.Request) {
 }
 
 func lastip(w http.ResponseWriter, r *http.Request) {
-	ip, err := os.ReadFile("ip.last")
+	// load config
+	configs, err := loadConfig("config.json")
+	if err != nil {
+		log.Printf("Error loading config: %v \n", err)
+	}
+	id := r.URL.Query().Get("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+	key := r.URL.Query().Get("key")
+	if configs.CLIENTS[idInt] != key {
+		http.Error(w, "Key Error", http.StatusBadRequest)
+		return
+	}
+
+	ip, err := os.ReadFile(id + ".ip")
 	if err != nil {
 		log.Println("error")
 		return
